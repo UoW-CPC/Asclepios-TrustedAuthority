a6fc8ed0f0c3a545dec6b2199b5ed6da2a90f045
hvandang@outlook.com <hvandang@outlook.com>
2020-08-06 22:03:42 +0100

    fix(change max length of charfield to adapt mysql)

--- api/models.py
+++ api/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 class FileNo(models.Model):
-    w = models.CharField(max_length=500,unique=True)
+    w = models.CharField(max_length=255,unique=True)
     fileno = models.IntegerField()
     def __str__(self):
         return '%s %d' % (self.w, self.fileno)
    
 class SearchNo(models.Model):
-    w = models.CharField(max_length=500,unique=True)
+    w = models.CharField(max_length=255,unique=True)
     searchno = models.IntegerField()
     def __str__(self):
         return '%s %d' % (self.w, self.searchno)
     
 class Key(models.Model):
-    key = models.CharField(max_length=256,unique=True)
+    key = models.CharField(max_length=255,unique=True)
     def __str__(self):
         return '%s %d' % (self.key)
