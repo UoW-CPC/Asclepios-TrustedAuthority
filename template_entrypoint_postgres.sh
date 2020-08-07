13584d9a0c611457a9522a0ce502ccab9c5ee32b
hvandang@outlook.com <hvandang@outlook.com>
2020-08-07 09:52:26 +0100

    feat(add templates for mysql and postgres)

---
+++ template_entrypoint_postgres.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Prepare log files and start outputting logs to stdout
+#touch ./logs/sse.log
+#touch ./logs/sse-access.log
+#tail -n 0 -f ./logs/sse*.log &
+
+#export DJANGO_SETTINGS_MODULE=sse.settings
+# Apply database migrations
+
+set -e
+
+# check if postgres server is launched
+until PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -h "$DB_HOST" -c '\q' "$DB_NAME"; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
+done
+
+>&2 echo "Postgres is up - starting service"
+
+echo "Apply database migrations"
+python3 manage.py makemigrations
+python3 manage.py migrate --run-syncdb
+
+exec gunicorn TA.wsgi:application \
+    --name TA \
+    --bind 0.0.0.0:8000 \
+    --workers 3
+    # --limit-request-line 0 # configure to allow long-line requests
+    # \
+    #--log-level=info \
+    #--log-file=./logs/sse.log \
+    #--access-logfile=./logs/sse-access.log \
+#"$@"
