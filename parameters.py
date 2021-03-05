d0c617fd8e154f36f304812f48496621f456534f
haivan <hvandang@outlook.com>
2021-03-05 12:59:34 +0000

    fix: remove SALT and ITER usage

--- parameters.py
+++ parameters.py
@@ -3,9 +3,9 @@ import os
 """ Configurable parameters """
 
 hash_length = int(os.getenv("HASH_LENGTH"))
-SALT = str.encode(os.getenv("SALT")) # string -> bytes
+#SALT = str.encode(os.getenv("SALT")) # string -> bytes
 IV = str.encode(os.getenv("IV")) # string -> bytes
 MODE = os.getenv("MODE") # cipher mode
 KS = int(os.getenv("KS"))# key size
-ITER = int(os.getenv("ITER")) # number of iterations to generate key from passphrase
+#ITER = int(os.getenv("ITER")) # number of iterations to generate key from passphrase
 TEEP_SERVER = os.getenv("TEEP_SERVER")
